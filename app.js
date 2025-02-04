cb5b6604cd2c6a812a49942b76f78dfda24cae4a
Arthur <ao.bergamascki@gmail.com>
2025-02-04 03:05:53 -0300

    Atualizações sem o arquivo database.db estranho

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+app.post('/AllData', async (req, res) =>{
+    const { name_table } =  req.body;
+    try {
+        const data = await db.getAllData(name_table); 
+        res.json(data); 
+    } catch (err) {
+        res.status(400).json({err: 'Erro no processamento de requisição AllData!'})
+    }
+})
 
 app.listen(port, () => {
     console.log("API em funcionamento ! - Porta: " + port);
--- scripts/db.js
+++ scripts/db.js
@@ -1,13 +1,136 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database(process.env.DB_PATH, (err) => {
+
+function connect() {
+  return new sqlite3.Database(process.env.DB_PATH, (err) => {
     if (err) {
       console.error('Erro ao conectar ao banco de dados:', err.message);
     } else {
       console.log('Conectado ao banco de dados SQLite.');
     }
   });
+}
+
+const createTables = () => {
+  const db = connect();
+
+  const createUsersTable = `
+    CREATE TABLE IF NOT EXISTS users (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      NAME TEXT NOT NULL,
+      IDADE INTEGER NOT NULL,
+      NASCIMENTO TEXT NOT NULL,
+      CARGO TEXT NOT NULL
+    );
+  `;
+  const createSkillsTable = `
+    CREATE TABLE IF NOT EXISTS skills (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      NOME TEXT NOT NULL,
+      NIVEL TEXT NOT NULL,
+      DESCRICAO TEXT,
+      IMG TEXT
+    );
+  `;
+  const createFeaturesTable = `
+    CREATE TABLE IF NOT EXISTS features (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      NOME_USER INTEGER,
+      CATEGORIA TEXT NOT NULL,
+      NOME_SKILLS INTEGER,
+      FOREIGN KEY (NOME_USER) REFERENCES users(ID),
+      FOREIGN KEY (NOME_SKILLS) REFERENCES skills(ID)
+    );
+  `;
+  const createContatoTable = `
+    CREATE TABLE IF NOT EXISTS contato (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      NOME_USER INTEGER,
+      TIPO TEXT NOT NULL,
+      INFO TEXT NOT NULL,
+      IMG TEXT,
+      FOREIGN KEY (NOME_USER) REFERENCES users(ID)
+    );
+  `;
+  const createProjetosTable = `
+    CREATE TABLE IF NOT EXISTS projetos (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      NOME_USER INTEGER,
+      NOME TEXT NOT NULL,
+      INFO TEXT NOT NULL,
+      IMG TEXT,
+      FOREIGN KEY (NOME_USER) REFERENCES users(ID)
+    );
+  `;
+  const createProjetosSkillsTable = `
+    CREATE TABLE IF NOT EXISTS projetos_skills (
+      PROJETO_ID INTEGER,
+      SKILL_ID INTEGER,
+      NOME_USER,
+      FOREIGN KEY (PROJETO_ID) REFERENCES projetos(ID),
+      FOREIGN KEY (SKILL_ID) REFERENCES skills(ID),
+      FOREIGN KEY (NOME_USER) REFERENCES users(ID),
+      PRIMARY KEY (PROJETO_ID, SKILL_ID)
+    );
+  `;
+
+  db.run(createUsersTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "users":', err.message);
+    }
+  });
+
+  db.run(createSkillsTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "skills":', err.message);
+    }
+  });
+
+  db.run(createFeaturesTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "features":', err.message);
+    }
+  });
+
+  db.run(createContatoTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "contato":', err.message);
+    }
+  });
+
+  db.run(createProjetosTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "projetos":', err.message);
+    }
+  });
+
+  db.run(createProjetosSkillsTable, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela "projetos_skills":', err.message);
+    }
+  });
+  db.close();
+};
+
+async function getAllData(tableName) {
+  const db = connect();
+  const query = `SELECT * FROM ${tableName}`;
+
+  return new Promise((resolve, reject) => {
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        reject(new Error(`Erro ao acessar a tabela ${tableName}: ${err.message}`));
+      } else {
+        resolve(rows);
+      }
+    });
+    db.close();
+  });
+}
+
+createTables()
+
 
 module.exports = {
-
+  getAllData
 };
